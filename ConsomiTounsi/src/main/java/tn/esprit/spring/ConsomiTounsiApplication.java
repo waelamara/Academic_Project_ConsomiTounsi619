5eb42e533eb8d60fceee33a82570d53180577c1a
waelbenamara <wael.benamara@esprit.tn>
2020-03-23 21:57:16 +0100

    Upload/Download CRUD Image

--- ConsomiTounsi/src/main/java/tn/esprit/spring/ConsomiTounsiApplication.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/ConsomiTounsiApplication.java
@@ -3,9 +3,13 @@ package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import Config.FileStorageProperties;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class ConsomiTounsiApplication {
 
 	public static void main(String[] args) {
