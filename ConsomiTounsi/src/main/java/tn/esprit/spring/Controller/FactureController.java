8275d97271a9fb701fba652096c1c51594cb0518
Iheb <Iheb.lassoued@esprit.tn>
2020-03-26 18:17:00 +0100

    facturepdf

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/FactureController.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/FactureController.java
@@ -1,7 +1,15 @@
 package tn.esprit.spring.Controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +21,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import tn.esprit.spring.DAO.CommandeDAO;
 import tn.esprit.spring.DAO.FactureDAO;
@@ -30,6 +44,8 @@ public class FactureController {
 	FactureDAO factureDAO;
 	@Autowired
 	CommandeDAO commandeDao;
+	@Autowired
+	ServletContext context;
 	@PostMapping("/ajouter/{idCommande}")
 	public ResponseEntity <Facture> AjouterFacture(@PathVariable(value = "idCommande") Long idCommande,@Valid @RequestBody Facture f) 
 			
@@ -61,5 +77,53 @@ public class FactureController {
 	
 		return factureDAO.FactureParIdUser(id);
 	}
+	@GetMapping("/afficherPDF//{idUser}")
+	public void createpdf(HttpServletRequest request ,HttpServletResponse reponse,@PathVariable(value = "idUser") long id) 
+	{
+	List<lignecommandeproduit> commandes =factureDAO.FactureParIdUser(id);
+	boolean flag = factureDAO.CreePdf(commandes, context, request, reponse);
+	if(flag)
+	{
+		String fullpath = request.getServletContext().getRealPath("/resources/report/" + "employees" + ".pdf");
+		fileDownload(fullpath,reponse,"employees.pdf");
+	
+	}
+	}
+	private void fileDownload(String fullpath,HttpServletResponse reponse,String fileName)
+	{
+		File f = new File(fullpath);
+		final int buffer = 4096;
+		
+		if(f.exists())
+		{
+		try{
+			FileInputStream inputStream = new FileInputStream(f);
+			String mineType = context.getMimeType(fullpath);
+			reponse.setContentType(mineType);
+			//reponse.setHeader(""content-disposition:inline;", "filename="+fileName);"
+					reponse.setHeader("content-disposition:inline; ", "filename="+ fileName);
+			OutputStream outputStream = reponse.getOutputStream();
+			byte[] buffers = new byte[buffer];
+			int bytesRead = -1;
+			while((bytesRead=inputStream.read(buffers))!=-1)
+			{
+				outputStream.write( buffers,0,bytesRead );
+			}
+			inputStream.close();
+			outputStream.close();
+			f.delete();
+		
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		}
+				
+	}
+	
+	
+	}
+	
 
-}
+
--- ConsomiTounsi/src/main/java/tn/esprit/spring/DAO/FactureDAO.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/DAO/FactureDAO.java
@@ -1,10 +1,23 @@
 package tn.esprit.spring.DAO;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import tn.esprit.spring.Model.Commande;
 import tn.esprit.spring.Model.Facture;
@@ -35,6 +48,60 @@ public void Delete(Facture f) {
 public List<lignecommandeproduit> FactureParIdUser( long id) {
 	return factureRepository.FactureParIdUser(id);
 
+}
+public boolean CreePdf(List<lignecommandeproduit>commandes ,ServletContext context,HttpServletRequest request ,HttpServletResponse reponse )
+{
+	try {
+	Document d = new Document(PageSize.A4,15,15,45,30);
+	
+String filePath = context.getRealPath("/resources/report");
+File file = new File(filePath);
+boolean exists =new File(filePath).exists();
+if (!exists)
+{
+	new File(filePath).mkdirs();
+	
+}
+
+PdfWriter writer =PdfWriter.getInstance(d, new FileOutputStream(file+"/"+"employees"+".pdf"));
+	d.open();
+	
+	//PdfPTable table = new PdfPTable(4);
+   // Font tableHeader = FontFactory.getFont("Arial", 10, BaseColor.BLACK);
+	for(lignecommandeproduit c : commandes)
+	{
+		
+		Phrase C = new Phrase("\n "+"nom de Produit:    "+c.getNomProduit()+"   "+"quantity:    "+c.getQuantity()+"     prix:   "+c.getPrice()+"     total:   "+c.getTotal());
+		
+		//Phrase C1 = new Phrase("nom:      "+c.getClient().getNomClient()+"\n");
+	
+		d.add(C);
+		//d.add(C1);
+		
+	
+		
+		
+	
+	
+		
+		//d.add(nameValue);
+	}
+	//PdfPCell nameValue  = new PdfPCell (new Paragraph(c.getStatus()));
+	
+	
+	  d.close();
+      writer.close();
+	  //outputStream.flush();
+      //outputStream.close();
+      return true;
+} catch (FileNotFoundException | DocumentException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+	
+	
+	return true;
 }
 
 }
