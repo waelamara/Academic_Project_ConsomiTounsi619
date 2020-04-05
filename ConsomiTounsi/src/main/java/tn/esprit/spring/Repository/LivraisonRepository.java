a350f15d4efd0b0dd06b787dcabe88b245389216
Ouss <21623@DESKTOP-QNELTRK>
2020-04-06 00:49:43 +0100

    CalculFraisLivraison

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Repository/LivraisonRepository.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Repository/LivraisonRepository.java
@@ -1,9 +1,15 @@
 package tn.esprit.spring.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tn.esprit.spring.Model.Livraison;
 
 public interface LivraisonRepository extends JpaRepository <Livraison, Long> {
+	
+	@Query(value = "SELECT  `produit_id` FROM `ligne_commande` WHERE `idcommande_id` =?1", nativeQuery = true)
+	public List<String> ListProduit_idcommande(float id_commande);
 
 }
