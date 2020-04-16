89a7711ea3eb82e41764c63ed8205332c43018f1
khayat1996 <44604659+khayat1996@users.noreply.github.com>
2020-04-16 20:44:26 +0100

    gestion charite

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Service/Charite/EventsDAOImpl.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Service/Charite/EventsDAOImpl.java
@@ -82,6 +82,25 @@ public class EventsDAOImpl implements EventsDAO {
 		// TODO Auto-generated method stub
 		
 	}
+	/*public class SmsSender {
+	    // Find your Account Sid and Auth Token at twilio.com/console
+	    public static final String ACCOUNT_SID =
+	            "AC25eeab7c940f79dd272d5bc2d7337437";
+	    public static final String AUTH_TOKEN =
+	            "your_auth_token";
+
+	    public static void main(String[] args) {
+	        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+	        Message message = Message
+	                .creator(new PhoneNumber("+14159352345"), // to
+	                        new PhoneNumber("+14158141829"), // from
+	                        "Where's Wallace?")
+	                .create();
+
+	        System.out.println(message.getSid());
+	    }
+	}*/
 
 	
 
