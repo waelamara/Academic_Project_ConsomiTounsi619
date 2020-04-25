d1a8258b42a3e8a803a5bc60c30d6a7f54b14c0c
Iheb <Iheb.lassoued@esprit.tn>
2020-04-25 05:09:33 +0200

    affichage Cart

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/CartController.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/CartController.java
@@ -1,12 +1,29 @@
 package tn.esprit.spring.Controller;
 
+import java.util.List;
+
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import tn.esprit.spring.Model.lignecommandeproduit;
+import tn.esprit.spring.Service.Panier.LigneCommandeImpl;
 
 @Controller(value = "cartController")
 @ELBeanName(value = "cartController")
 @Join(path = "/Cart", to = "/Cart.jsf")
 public class CartController {
+	
+	@Autowired
+	LigneCommandeImpl ligneCommandeDao;
+	
+	
+	public List<lignecommandeproduit> panierParIdclient(long id)
+	{
+		
+		return ligneCommandeDao.panierParIdclient(id);
+	}
+	
 
 }
