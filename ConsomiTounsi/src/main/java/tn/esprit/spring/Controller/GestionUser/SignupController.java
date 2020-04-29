e726c01b06c63902ed312b2d68fe0988559b0c62
raed <raed.bahri@esprit.tn>
2020-04-29 08:17:18 +0200

    Raed Signup

---
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/GestionUser/SignupController.java
@@ -0,0 +1,192 @@
+package tn.esprit.spring.Controller.GestionUser;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import tn.esprit.spring.Model.ERole;
+import tn.esprit.spring.Model.Role;
+import tn.esprit.spring.Model.Sexe;
+import tn.esprit.spring.Model.User;
+import tn.esprit.spring.Repository.RoleRepository;
+import tn.esprit.spring.Repository.UserRepository;
+import tn.esprit.spring.Service.GestionUser.OnRegistrationCompleteEvent;
+import tn.esprit.spring.payload.response.MessageResponse;
+
+@Controller(value = "signupController")
+@ELBeanName(value = "signupController")
+@Join(path = "/signup", to = "/signup.jsf")
+@Component
+public class SignupController {
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+	@Autowired
+	ApplicationEventPublisher eventPublisher;
+	
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String email;
+	private String address;
+    private Date dateN;
+	private String tel;
+ 	private Sexe sexe;
+
+	public String doSignup() {
+		String navigateTo = "null";
+		if (userRepository.existsByUsername(username)) {
+			FacesMessage facesMessage =
+
+					new FacesMessage("Error: Username is already taken!");
+
+					FacesContext.getCurrentInstance().addMessage("form1:btn",facesMessage);
+		}
+
+		else if (userRepository.existsByEmail(email)) {
+			FacesMessage facesMessage =
+
+					new FacesMessage("Error: Email is already in use!");
+
+					FacesContext.getCurrentInstance().addMessage("form1:btn",facesMessage);
+		}
+		else
+		{
+			User user = new User(username, 
+					 email,
+					 encoder.encode(password),firstName
+					 ,lastName,address,dateN,tel
+					 ,sexe);
+			Set<Role> roles = new HashSet<>();
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+			user.setRoles(roles);
+			userRepository.save(user);
+			String appUrl = "";
+			User registered= user;
+			eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, appUrl));
+			navigateTo="login.xhtml?faces-redirect=true";
+			FacesMessage facesMessage =
+
+					new FacesMessage("Registered successfully, please verify your account!");
+
+					FacesContext.getCurrentInstance().addMessage("form:btn",facesMessage);
+		}
+		return navigateTo;
+		
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public Date getDateN() {
+		return dateN;
+	}
+
+
+	public void setDateN(Date dateN) {
+		this.dateN = dateN;
+	}
+
+
+	public String getTel() {
+		return tel;
+	}
+
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+
+	public Sexe getSexe() {
+		return sexe;
+	}
+
+
+	public void setSexe(Sexe sexe) {
+		this.sexe = sexe;
+	}
+	
+	
+	
+	
+
+}
