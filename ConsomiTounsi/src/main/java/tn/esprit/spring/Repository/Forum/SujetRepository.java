c4cfae706406d66432b69f70706d8ee79245675f
aymen ayed <aymen ayed@DESKTOP-U22SC4K>
2020-05-18 01:27:31 +0100

    conflit

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Repository/Forum/SujetRepository.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Repository/Forum/SujetRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tn.esprit.spring.Repository.Forum;
 
 import java.util.List;
@@ -21,27 +20,5 @@ public interface SujetRepository extends JpaRepository<Sujet, Long> {
 	
 	@Query(value="SELECT count(*) FROM `sujet` WHERE `id_categorie_sujet`=?1",nativeQuery=true)
 	public int countSujetbycatId(Long categId);
-=======
-package tn.esprit.spring.Repository.Forum;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import tn.esprit.spring.Model.Forum.*;
-
-public interface SujetRepository extends JpaRepository<Sujet, Long> {
-	@Query(value = "SELECT DISTINCT * FROM sujet WHERE nom_sujet LIKE ?1%", nativeQuery = true)
-	public List<Sujet> findSujetbyName(String name);
-
-	@Query(value = "SELECT DISTINCT * FROM sujet ORDER BY nb_like Desc,date_ajout DESC", nativeQuery = true)
-	public List<Sujet> findAllOrderbyDate();
-
-	@Query(value = "SELECT * FROM sujet WHERE id_categorie_sujet= ?1 ORDER BY nb_like Desc,date_ajout DESC ", nativeQuery = true)
-	public List<Sujet> findSujetbycatgID(Long categId);
-
-	@Query(value = "SELECT id_categorie_sujet,count(*) as nb FROM sujet GROUP by id_categorie_sujet order by nb desc", nativeQuery = true)
-	public List<NbSujetbyCat> countSujetbycatId();
->>>>>>> branch 'master' of https://github.com/waelamara/ConsomiTounsi619.git
 }
