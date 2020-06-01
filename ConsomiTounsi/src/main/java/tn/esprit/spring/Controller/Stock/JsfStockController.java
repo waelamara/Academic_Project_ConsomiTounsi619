06d078f7666596fa1818f1287b0ca84b2c86c630
Heni <Heni@DESKTOP-HS8DEL2>
2020-06-01 21:19:10 +0200

    hen

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/Stock/JsfStockController.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/Stock/JsfStockController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package tn.esprit.spring.Controller.Stock;
 
 import java.util.Date;
@@ -103,34 +103,3 @@ public int getQuantite() {
 		}
 
 }
-=======
-package tn.esprit.spring.Controller.Stock;
-
-import java.util.List;
-
-import org.ocpsoft.rewrite.annotation.Join;
-import org.ocpsoft.rewrite.el.ELBeanName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-import tn.esprit.spring.Model.Stock.Stock;
-import tn.esprit.spring.Service.Stock.IStockService;
-
-
-
-
-@Controller(value = "JsfStockController")
-@ELBeanName(value = "JsfStockController")
-@Join(path = "/stock", to = "/stock.jsf")
-public class JsfStockController {
-	
-	@Autowired
-	IStockService stockservice;
-	
-	
-	public List<Stock> allStock() {
-		return stockservice.allStock();
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/waelamara/ConsomiTounsi619
