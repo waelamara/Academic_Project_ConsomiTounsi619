834e32581266dae414edee523ec8e2fd46ae6089
waelamara <wael.benamara@esprit.tn>
2020-06-06 18:21:56 +0200

    Gestion Publicity (Admin)

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Service/Publicite/IPubliciteService.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Service/Publicite/IPubliciteService.java
@@ -3,8 +3,10 @@ package tn.esprit.spring.Service.Publicite;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
+import org.primefaces.model.file.UploadedFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,8 +17,10 @@ import tn.esprit.spring.Model.Publicite.Publicite;
 public interface IPubliciteService {
 	public Publicite save(Publicite p);
 	public Publicite Add(String ProduitJson,MultipartFile file)throws JsonMappingException, JsonProcessingException,IOException, ParseException;
+	public Publicite AddPub(Publicite pub,UploadedFile file) throws IOException,ParseException;
 	public List<Publicite> findAll();
 	public Publicite Update(Publicite p,Long id);
+	public Publicite UpdatePubWithoutImage(Publicite pub) throws IOException, ParseException;
 	public void Delete(Long id);
 	public Publicite findOne(Long id);
 	public float CalculeCoutTotalPub(String gender, String canal, int ageCibledebut, int ageCibleFin, String dateDebut,
@@ -25,6 +29,10 @@ public interface IPubliciteService {
 	public int DifferenceJourDateDebutEtDateFin(String dateDebut, String dateFin) throws ParseException;
 	public int CoutSurCanal(String canal);
 	public int CoutSurTrancheAge(int ageCibledebut, int ageCibleFin);
-	public String TypeFile(MultipartFile file) throws IOException;
 	public Publicite AffecterImageVideoPub(Publicite pub, MultipartFile file) throws IOException;
+	public List<Publicite> findLikeName(String nom);
+	public List<Publicite> findByCanal(String canal);
+	public Date ConvertirDate(String date) throws ParseException;
+	public String TypeFile(UploadedFile file) throws IOException;
+	
 }
