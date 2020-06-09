c1dafb83f2eda9b6f50efea81a709b0c62a8f2b3
Ouss <21623@DESKTOP-QNELTRK>
2020-06-09 09:36:07 +0100

    Reclamation admin

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/ComplaintAdminController.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/ComplaintAdminController.java
@@ -2,26 +2,59 @@ package tn.esprit.spring.Controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import tn.esprit.spring.Model.reclamation;
+import tn.esprit.spring.Repository.reclamationRepository;
 import tn.esprit.spring.Service.Reclamation.ReclamationService;
 
 
 @Controller(value = "ComplaintAdminController")
 @ELBeanName(value = "ComplaintAdminController")
-@Join(path = "/showComplaint", to = "/ComplaintAdmin.jsf")
+@Join(path = "/showComplaint", to = "/ReclamationAdmin.jsf")
 public class ComplaintAdminController {
 	
 	@Autowired
 	ReclamationService ReclamationService;
+	@Autowired
+	reclamationRepository r;
 	
 	/*get all reclamation*/
 	public List<reclamation> getAllrec(){
 		
 		return ReclamationService.findall();
+		
+	}
+	/*Rembourser reclamation*/
+	@Transactional
+	public String rembourserReclamation(long id){
+		String navigateTo = "/ReclamationAdmin.xhtml"; 
+		r.ChangetEtat("Remboursement", id);
+		return navigateTo;
+		
+		
+	}
+	/*Echange reclamation*/
+	@Transactional
+	public String echangeReclamation(long id){
+		String navigateTo = "/ReclamationAdmin.xhtml"; 
+		r.ChangetEtat("Echange", id);
+		return navigateTo;
+		
+		
+	}
+	/*Reparer reclamation*/
+	@Transactional
+	public String reparerReclamation(long id){
+		String navigateTo = "/ReclamationAdmin.xhtml"; 
+		r.ChangetEtat("Réparation", id);
+		return navigateTo;
+		
 		
 	}
 	
--- ConsomiTounsi/src/main/java/tn/esprit/spring/Repository/reclamationRepository.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Repository/reclamationRepository.java
@@ -1,11 +1,18 @@
 package tn.esprit.spring.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import tn.esprit.spring.Model.reclamation;
 
 
 
 public interface reclamationRepository extends JpaRepository <reclamation, Long> {
+	
+	/* Change Etat Reclamation */
+	@Modifying
+	@Query(value = "UPDATE `reclamation` SET `etat`=?1,`traiter`=1 WHERE `id`=?2", nativeQuery = true)
+	public void ChangetEtat(String Dispo, long id);
 
 }
