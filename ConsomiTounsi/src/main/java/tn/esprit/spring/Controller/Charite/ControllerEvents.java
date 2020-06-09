7480707fb06415a5b60fffe6c2ce055c074950b7
khayat1996 <44604659+khayat1996@users.noreply.github.com>
2020-06-09 23:39:12 +0100

    gestion charite

--- ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/Charite/ControllerEvents.java
+++ ConsomiTounsi/src/main/java/tn/esprit/spring/Controller/Charite/ControllerEvents.java
@@ -84,8 +84,8 @@ public class ControllerEvents {
 	PubDAO publiciteDAO;
 	@Autowired
 	FileStorageServiceImpl fileStorageServiceImpl;
-	public static final String ACCOUNT_SID = "AC25eeab7c940f79dd272d5bc2d7337437";
-	  public static final String AUTH_TOKEN = "cf00808dd9240106de0943465ae7408e";
+	public static final String ACCOUNT_SID = "ACc175ca36e782887ed7bf1fed9915084f";
+	  public static final String AUTH_TOKEN = "6cba54a73ba1df6bed005ae00960daad";
 	
 	private Long Id;
 	private String titre;
